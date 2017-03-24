4c2e09f7bb9be211452c4023f631877e8d67c084
Maarten van den Berg <maartenberg1@gmail.com>
2017-03-24 09:48:57 +0100

    DO NOT MERGE TO MASTER Point to development koala

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
 
-render(<App apiRoot='https://koala.svsticky.nl/api'/>, document.getElementById('root'));
+render(<App apiRoot='http://koala.rails.dev:3000/api'/>, document.getElementById('root'));
